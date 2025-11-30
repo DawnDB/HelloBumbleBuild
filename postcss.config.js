e62b29e44dbb3398f1dbe5c9d21c7f1eb668dfb8
DawnDB <didibotha2020@gmail.com>
2025-11-30 14:43:00 +0200

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      // This will ensure Tailwind uses your tailwind.config.js
+      config: './tailwind.config.js',
+    },
+    autoprefixer: {},
+  },
+};
