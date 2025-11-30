40acba5a5e72b6ec89a46f73e8fdd5af77ae03cc
DawnDB <didibotha2020@gmail.com>
2025-12-01 00:24:11 +0200

    Create page.js

---
+++ app/about/helpamama/hmtc/page.js
@@ -0,0 +1,21 @@
+export function HMTCTerms() {
+return (
+<div className="min-h-screen px-6 py-16 text-blackText">
+<div className="max-w-3xl mx-auto bg-whiteOverlay backdrop-blur-sm rounded-2xl p-6 shadow">
+<h1 className="text-4xl mb-6 font-hellobumble">HelloBumble Terms and Conditions</h1>
+
+
+<p className="font-description leading-relaxed">
+Help-a-mama nappies are ROAK nappies and not allowed to be sold, once they become help-a-mama nappies they should live on to be free to a mama and cloth bum in need. Please honour this by either donating your help-a-mama nappies back or to a friend in need. Please do not dishonour the sentiment by making another mama pay for what you were blessed with.
+<br /><br />
+Help-a-mama nappies will be in different conditions than the pre-loved nappies, they might have a light stain or so, but we will continue to ensure that they go to their new home fully functional.
+<br /><br />
+The process is the same as the pre-loved, except no store credit will be given for the donated nappies and we refund you half of the shipping (if the nappies are still in good enough condition for their next home) upon inspection and receipt of the shipping cost statement.
+</p>
+
+
+<a href="/helpamama" className="btn btn-size mt-6 inline-block">Back to HaM</a>
+</div>
+</div>
+);
+}
