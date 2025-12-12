33732429a9543615319d5e2a9ac48ad583bff0ba
DawnDB <didibotha2020@gmail.com>
2025-12-12 20:02:37 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -118,13 +118,24 @@ function Header() {
 function MobileMenu({ closeMenu }) {
   return (
     <nav className="flex flex-col gap-4 mt-2 text-lg">
-      <Link href="/" onClick={closeMenu}>Contact</a>
+      <Link href="/" onClick={closeMenu}>Home</Link>
+
+      <p className="font-product font-semibold mt-4">About</p>
+      <Link href="/about/hellobumble" className="ml-4" onClick={closeMenu}>HelloBumble</Link>
+      <Link href="/about/pandm" className="ml-4" onClick={closeMenu}>Products & Materials</Link>
+      <Link href="/ambuzzador" className="ml-4" onClick={closeMenu}>AmbuZZador</Link>
+      <Link href="/preloved" className="ml-4" onClick={closeMenu}>Pre-Loved</Link>
+      <Link href="/about/helpamama" className="ml-4" onClick={closeMenu}>Help-a-Mama</Link>
+      <a href="#footer-contact" onClick={closeMenu}>Contact</a>
 
       <p className="font-product font-semibold mt-4">Shop</p>
       <Link href="/shop/diapering" className="ml-4" onClick={closeMenu}>Diapering</Link>
       <Link href="/shop/essentials" className="ml-4" onClick={closeMenu}>Essentials</Link>
       <Link href="/shop/mom" className="ml-4" onClick={closeMenu}>Mom</Link>
+
+      {/* SECOND pre-loved link (on purpose, as you said!) */}
       <Link href="/preloved" className="ml-4" onClick={closeMenu}>Pre-Loved</Link>
+
       <Link href="/profile" className="ml-4" onClick={closeMenu}>Profile</Link>
     </nav>
   );
