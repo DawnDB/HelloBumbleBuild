d8c7a1d6e5d1a16aa2343a305135fe410567ff18
DawnDB <didibotha2020@gmail.com>
2025-12-17 15:15:34 +0200

    Create data/products/diapering.js

---
+++ app/data/products/diapering.js
@@ -0,0 +1,17 @@
+export const diaperingProducts = [
+  {
+    slug: "product1",
+    name: "Product One",
+    image: "/images/products/product1.jpg",
+  },
+  {
+    slug: "product2",
+    name: "Product Two",
+    image: "/images/products/product2.jpg",
+  },
+  {
+    slug: "product3",
+    name: "Product Three",
+    image: "/images/products/product3.jpg",
+  },
+];
