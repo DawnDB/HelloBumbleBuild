209f55a5ee75e8244cf8a64b9854b93b155a83cb
DawnDB <didibotha2020@gmail.com>
2025-12-17 14:51:01 +0200

    Update page.js

--- app/about/helpamama/hmtc/page.js
+++ app/about/helpamama/hmtc/page.js
@@ -1,21 +1,36 @@
 export function HMTCTerms() {
-return (
-<div classNName="min-h-screen bg-[url('/pastel-marble.jpg')] bg-cover bg-center bg-no-repeat px-6 py-16 text-blackText">
-      <div className="max-w-3xl mx-auto bg-whiteOverlay backdrop-blur-sm rounded-2xl p-6 shadow">
-<h1 className="text-4xl mb-6 font-hellobumble">HelloBumble Terms and Conditions</h1>
+  return (
+    <div className="min-h-screen px-6 py-20 flex flex-col items-center font-description">
+      <div className="w-full max-w-3xl bg-neutral-whiteOverlay rounded-2xl shadow-soft p-10 text-center">
+        <h1 className="text-5xl mb-6 font-hellobumble text-neutral-blackText">
+          HelloBumble Terms and Conditions
+        </h1>
 
+        <p className="leading-relaxed text-left">
+          Help-a-Mama nappies are ROAK nappies and are not allowed to be sold.
+          Once they become Help-a-Mama nappies, they should live on freely to
+          another mama and cloth bum in need. Please honour this by either
+          donating your Help-a-Mama nappies back or passing them on to a friend
+          in need. Please do not dishonour the sentiment by making another mama
+          pay for what you were blessed with.
+          <br /><br />
+          Help-a-Mama nappies may be in a different condition than Pre-Loved
+          nappies. They might have light staining, but we ensure they are always
+          fully functional before going to their next home.
+          <br /><br />
+          The process is similar to Pre-Loved, except no store credit is given
+          for donated nappies. We will refund half of the shipping cost (if the
+          nappies are still in good enough condition for their next home) upon
+          inspection and receipt of the shipping cost statement.
+        </p>
 
-<p className="font-description leading-relaxed">
-Help-a-mama nappies are ROAK nappies and not allowed to be sold, once they become help-a-mama nappies they should live on to be free to a mama and cloth bum in need. Please honour this by either donating your help-a-mama nappies back or to a friend in need. Please do not dishonour the sentiment by making another mama pay for what you were blessed with.
-<br /><br />
-Help-a-mama nappies will be in different conditions than the pre-loved nappies, they might have a light stain or so, but we will continue to ensure that they go to their new home fully functional.
-<br /><br />
-The process is the same as the pre-loved, except no store credit will be given for the donated nappies and we refund you half of the shipping (if the nappies are still in good enough condition for their next home) upon inspection and receipt of the shipping cost statement.
-</p>
-
-
-<a href="/helpamama" className="btn btn-size mt-6 inline-block">Back to HaM</a>
-</div>
-</div>
-);
+        <a
+          href="/helpamama"
+          className="btn-primary mt-8 inline-block"
+        >
+          Back to Help-a-Mama
+        </a>
+      </div>
+    </div>
+  );
 }
