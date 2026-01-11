551989036848b32a073591a9e7e766d392035a2a
DawnDB <didibotha2020@gmail.com>
2026-01-11 15:31:16 +0200

    Create app/lib/email.js

---
+++ app/lib/email.js
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_SECURE === "true",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export async function sendEmail({ to, subject, html }) {
+  await transporter.sendMail({
+    from: `"HelloBumble 🐝" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+}
