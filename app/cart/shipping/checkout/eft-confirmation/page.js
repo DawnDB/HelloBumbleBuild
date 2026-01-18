5bc9cf80331587461bc3db2ad3e58aecd0e2e302
DawnDB <didibotha2020@gmail.com>
2026-01-18 19:00:16 +0200

    Update page.js
    refractor eftconfirmation

--- app/cart/shipping/checkout/eft-confirmation/page.js
+++ app/cart/shipping/checkout/eft-confirmation/page.js
@@ -1,18 +1,7 @@
-import { Suspense } from "react";
+"use client";
+
 import EFTConfirmationClient from "./EFTConfirmationClient";
 
 export default function EFTConfirmationPage() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <EFTConfirmationClient />
-    </Suspense>
-  );
-}
-
-function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p className="opacity-70">Loading payment details…</p>
-    </div>
-  );
+  return <EFTConfirmationClient />;
 }
